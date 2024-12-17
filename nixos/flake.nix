6f6dc7c1987b431f10164e441ef06582e20ebd29
rae <rae@roxwize.xyz>
2024-12-17 16:26:47 -0500

    ~

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -11,7 +11,7 @@
 
     outputs = { self, nixpkgs, home-manager, ... }@inputs: {
         nixosConfigurations = {
-            gayfurries = nixpkgs.lib.nixosSystem {
+            qemu = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
                 specialArgs = { inherit inputs; };
                 modules = [

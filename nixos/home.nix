2c56562cfed4a2c5b9ac41288d64b13fe3e02880
roxwize <rae@roxwize.xyz>
2024-12-17 20:01:32 -0500

    ~

--- nixos/home.nix
+++ nixos/home.nix
@@ -133,13 +133,29 @@
 
     dconf.settings = {
         "net/launchpad/plank/docks/dock1" = {
-            dock-items = ["firefox.dockitem" "io.elementary.terminal.dockitem" "codium.dockitem" "io.elementary.settings.dockitem"];
+            dock-items = [
+                "gala-multitaskingview.dockitem"
+                "firefox.dockitem"
+                "io.elementary.terminal.dockitem"
+                "codium.dockitem"
+                "io.elementary.settings.dockitem"
+            ];
         };
         "org/gnome/desktop/background" = {
             picture-uri = "file://" + builtins.toString /home/rae/.dotfiles/assets/wallpapers/current;
         };
+        "org/gnome/desktop/interface" = {
+            accent-color = "blue";
+            color-scheme = "prefer-dark";
+        };
         "org/gnome/desktop/wm/preferences" = {
             button-layout = ":minimize,maximize,close";
+        };
+        "org/pantheon/desktop/gala/appearance" = {
+            button-layout = ":minimize,maximize,close";
+        };
+        "org/pantheon/desktop/gala/behavior" = {
+            hotcorner-bottomright = "show-workspace-view";
         };
     };
 

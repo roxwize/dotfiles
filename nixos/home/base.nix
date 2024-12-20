573bf2337fa3f058f9b258eb45ed698e84b8206b
roxwize <rae@roxwize.xyz>
2024-12-19 22:47:22 -0500

    ~

--- nixos/home/base.nix
+++ nixos/home/base.nix
@@ -128,7 +128,7 @@
                 vim.opt.tabstop = 4
                 vim.opt.softtabstop = 4
                 vim.opt.shiftwidth = 4
-                vim.expandtab = true
+                vim.opt.expandtab = true
             '';
         };
     };

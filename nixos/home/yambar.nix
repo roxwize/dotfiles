925d3ba4933d6bc51322b9c69345f827abdc9e50
roxwize <rae@roxwize.xyz>
2024-12-31 03:41:03 -0500

    ~

--- nixos/home/yambar.nix
+++ nixos/home/yambar.nix
@@ -4,19 +4,50 @@
         settings = {
             bar = {
                 location = "top";
-                height = 26;
-                background = "00000066";
+                height = 18;
+                background = "00000088";
+                font = "GohuFont:style=Regular:pixelsize=11";
+                margin = 16;
+                spacing = 3;
+
+                left = [
+                    {
+                        label = {
+                            content.string.text = "god's in his heaven | all's right with the world";
+                        };
+                    }
+                ];
 
                 right = [
+                    {
+                        pipewire = {
+                            content.map.conditions."type == sink".string = {
+                                text = "vol {cubic_volume}% |";
+                                on-click = {
+                                    left = "pavucontrol";
+                                    wheel-up = "amixer sset Master 1%+";
+                                    wheel-down = "amixer sset Master 1%-";
+                                };
+                            };
+                        };
+                    }
+                    {
+                        cpu = {
+                            poll-interval = 2000;
+                            content.map.conditions."id < 0".string.text = "cpu {cpu}%";
+                        };
+                    }
+                    {
+                        mem = {
+                            poll-interval = 2000;
+                            content.string.text = "ram {percent_used}% |";
+                        };
+                    }
                     {
                         clock = {
                             date-format = "%b %d %y";
                             time-format = "%I:%M %p";
-                            content = [
-                                {
-                                    string.text = "{date} {time}";
-                                }
-                            ];
+                            content.string.text = "{date} {time}";
                         };
                     }
                 ];

1739b89ea3bcadd1b221a24db1a9f6e39db6a1e5
roxwize <rae@roxwize.xyz>
2025-03-05 07:46:56 -0500

    ~

--- nixos/docker/default.nix
+++ nixos/docker/default.nix
@@ -1,12 +1,5 @@
-{ lib, config, ... }: with lib; let
-	c = options.containers;
-in {
-	options.containers = {
-		homepage = {
-			#TODO: https://github.com/NixOS/nixpkgs/blob/nixos-24.11/nixos/modules/services/misc/homepage-dashboard.nix#L221
-			enable = mkEnableOption "homepage";
-		};
-	};
-
-	config = mkIf c.homepage import ./homepage;
+{ lib, config, ... }: {
+	imports = [
+		./homepage
+	];
 }

9e407cd78f18cfa9cd0aa296c7eddf288a3433ec
roxwize <rae@roxwize.xyz>
2025-03-07 21:21:47 -0500

    ~

--- nixos/docker/pihole/default.nix
+++ nixos/docker/pihole/default.nix
@@ -3,21 +3,29 @@
 in with lib; {
 	options.r5e.containers.pihole = {
 		enable = mkEnableOption "pihole";
+		openFirewall = mkOption {
+			type = types.bool;
+			default = false;
+		};
 		api-password = mkOption {
-			type = types.string;
+			type = types.str;
 			default = "";
 		};
 	};
 
-	config = mkIf cfg.enable mkMerge [
+	config = mkIf cfg.enable (mkMerge [
 		(import ./docker-compose.nix { inherit pkgs lib; })
 		{
 			virtualisation.oci-containers.containers.pihole = {
 				environment = {
 					TZ = config.time.timeZone;
-					FTLCONF_webserver_api_password = mkIf (cfg.api-password != null) cfg.api-password;
+					FTLCONF_webserver_api_password = mkIf (cfg.api-password != "") cfg.api-password;
 				};
 			};
+
+			networking.firewall = mkIf cfg.openFirewall {
+				allowedTCPPorts = [ 53 80 443 ];
+			};
 		}
-	];
+	]);
 }
